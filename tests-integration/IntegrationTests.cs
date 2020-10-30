2e2b788a00c54c81bc3ac09346b2c57204e8311d
Carlos Peix <carlos.peix@gmail.com>
2020-10-29 21:48:58 -0300

    Implementación mas compacta basada en NewtonSoft

--- tests-integration/IntegrationTests.cs
+++ tests-integration/IntegrationTests.cs
@@ -1,4 +1,5 @@
-using System.Text.Json;
+using System.Net.Http;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using OpenChat.Api;
@@ -9,34 +10,25 @@ namespace OpenChat.Tests.Integration
     [Collection("Integration Tests")]
     public class IntegrationTests
     {
-        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _client;
 
         public IntegrationTests(WebApplicationFactory<Startup> factory)
         {
-            _factory = factory;
+            _client = factory.CreateClient();
         }
 
         [Fact]
         public async Task GetRoot_ReturnsSuccessAndStatusUp()
         {
-            // Arrange
-            var client = _factory.CreateClient();
-
             // Act
-            var response = await client.GetAsync("/");
+            var response = await _client.GetAsync("/");
 
             // Assert
             response.EnsureSuccessStatusCode();
-            Assert.NotNull(response.Content);
-            var statusObject = JsonSerializer.Deserialize<ResponseType>(
-                await response.Content.ReadAsStringAsync(),
-                new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
-            Assert.Equal("Up", statusObject.Status);
-        }
-
-        private class ResponseType
-        {
-            public string Status { get; set; }
+            var responseObject = JsonConvert.DeserializeObject<dynamic>(
+                await response.Content.ReadAsStringAsync());
+            
+            Assert.Equal("Up", (string)responseObject.status);
         }
     }
 }
