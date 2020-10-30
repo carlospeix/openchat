a03f1090f69f1cfeaba64b2ac0243dd01ed71816
Carlos Peix <carlos.peix@gmail.com>
2020-10-30 11:09:11 -0300

    User_RegisterNewUserSuccess (test failing)

--- tests-integration/IntegrationTests.cs
+++ tests-integration/IntegrationTests.cs
@@ -4,9 +4,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using OpenChat.Api;
 using Xunit;
+using System.Net;
+using System;
+using System.Text;
+using System.Dynamic;
 
 namespace OpenChat.Tests.Integration
 {
+    // Integration tests definition
+    // https://github.com/sandromancuso/cleancoders_openchat/blob/master/APIs.md
+
     [Collection("Integration Tests")]
     public class IntegrationTests
     {
@@ -18,17 +25,51 @@ namespace OpenChat.Tests.Integration
         }
 
         [Fact]
+        // POST - openchat/registration { "username" : "Alice", "password" : "alki324d", "about" : "I love playing the piano and travelling." }
+        // Success Status CREATED - 201 Response: { "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" "username" : "Alice", "about" : "I love playing the piano and travelling." }
+        // Failure Status: BAD_REQUEST - 400 Response: "Username already in use."
+        public async Task User_RegisterNewUserSuccess()
+        {
+            // Arrange
+            var content = GetContentFrom(new
+            {
+                username = "Alice",
+                password = "alki324d",
+                about = "I love playing the piano and travelling."
+            });
+
+            // Act
+            var httpResponse = await _client.PostAsync("/openchat/registration", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
+            dynamic response = await GetResponseFrom(httpResponse);
+            Assert.NotEqual(Guid.Empty, Guid.Parse(response.userId));
+            Assert.Equal("Alice", (string)response.username);
+            Assert.Equal("I love playing the piano and travelling.", (string)response.about);
+        }
+
+        [Fact]
         public async Task GetRoot_ReturnsSuccessAndStatusUp()
         {
             // Act
-            var response = await _client.GetAsync("/");
+            var httpResponse = await _client.GetAsync("/");
 
             // Assert
-            response.EnsureSuccessStatusCode();
-            var responseObject = JsonConvert.DeserializeObject<dynamic>(
-                await response.Content.ReadAsStringAsync());
-            
-            Assert.Equal("Up", (string)responseObject.status);
+            httpResponse.EnsureSuccessStatusCode();
+            dynamic response = await GetResponseFrom(httpResponse);
+            Assert.Equal("Up", (string)response.status);
+        }
+
+        private HttpContent GetContentFrom(object content)
+        {
+            return new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+        }
+
+        private async Task<dynamic> GetResponseFrom(HttpResponseMessage httpResponse)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(
+                await httpResponse.Content.ReadAsStringAsync());
         }
     }
 }
