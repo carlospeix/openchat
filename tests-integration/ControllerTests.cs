3061d2b73c935ba1f765cf703d5460cb5c556106
Carlos Peix <carlos.peix@gmail.com>
2020-11-02 18:54:44 -0300

    Preparado para el resto de los tests funcionales

--- tests-integration/ControllerTests.cs
+++ tests-integration/ControllerTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using OpenChat.Api;
 using OpenChat.Api.Controllers;
 using Xunit;
@@ -21,9 +18,11 @@ namespace OpenChat.Tests.Integration
             aliceRegistrationRequest = new RegistrationRequest("Alice", "alki324d", "I love playing the piano and travelling.");
         }
 
-        [Fact]
+        // Register New User
         // POST - openchat/registration { "username" : "Alice", "password" : "alki324d", "about" : "I love playing the piano and travelling." }
         // Success Status CREATED - 201 Response: { "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" "username" : "Alice", "about" : "I love playing the piano and travelling." }
+        // Failure Status: BAD_REQUEST - 400 Response: "Username already in use."
+        [Fact]
         public void User_RegisterNewUserSuccess()
         {
             // Act
@@ -36,13 +35,8 @@ namespace OpenChat.Tests.Integration
             Assert.Equal(aliceRegistrationRequest.username, userResult.username);
             Assert.Equal(aliceRegistrationRequest.about, userResult.about);
         }
-
         [Fact]
-        // POST - openchat/registration { "username" : "Alice", "password" : "alki324d", "about" : "I love playing piano." }
-        // Success Status CREATED - 201 Response: { "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" "username" : "Alice", "about" : "I love playing piano." }
-        // POST - openchat/registration { "username" : "Alice", "password" : "alki324d", "about" : "I love playing chess." }
-        // Failure Status: BAD_REQUEST - 400 Response: "Username already in use."
-        public void User_RegisterSameUserTwiceFails()
+        public void User_RegisterSameUserTwiceFailure()
         {
             // Arrange
             _ = controller.Registration(aliceRegistrationRequest) as ObjectResult;
@@ -55,10 +49,11 @@ namespace OpenChat.Tests.Integration
             Assert.Equal("Username already in use.", result.Value);
         }
 
-        [Fact]
+        // Login
         // POST - openchat/login { "username" : "Alice" "password" : "alki324d" }
         // Success Status OK - 200 Response: { "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" "username" : "Alice", "about" : "I love playing the piano and travelling." }
         // Failure Status: BAD_REQUEST - 400 Response: "Invalid credentials."
+        [Fact]
         public void User_LoginSuccess()
         {
             // Arrange
@@ -74,11 +69,8 @@ namespace OpenChat.Tests.Integration
             Assert.Equal(aliceRegistrationRequest.username, userResult.username);
             Assert.Equal(aliceRegistrationRequest.about, userResult.about);
         }
-
         [Fact]
-        // POST - openchat/login { "username" : "Alice" "password-X" : "alki324d" }
-        // Failure Status: BAD_REQUEST - 400 Response: "Invalid credentials."
-        public void User_LoginFail()
+        public void User_LoginFailure()
         {
             // Arrange
             _ = controller.Registration(aliceRegistrationRequest) as ObjectResult;
@@ -91,5 +83,47 @@ namespace OpenChat.Tests.Integration
             Assert.Equal(HttpStatusCode.BadRequest, (HttpStatusCode)result.StatusCode);
             Assert.Equal("Invalid credentials.", result.Value);
         }
+
+        // Create Post
+        // POST openchat/user//posts { "text" : "Hello everyone. I'm Alice." } Success Status CREATED - 201 { "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Hello everyone. I'm Alice.", "date" : "10/01/2018", "time" : "11:30:00" }
+        // Failure Status: BAD_REQUEST - 400 (in case user does not exist) Response: "User does not exit."
+
+
+
+
+        // Retrieve Posts (User timeline)
+        // GET - openchat/user//timeline [{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Anything interesting happening tonight?", "date" : "10/01/2018", "time" : "11:30:00" },{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Hello everyone. I'm Alice.", "date" : "10/01/2018", "time" : "09:00:00" }]
+        // Success Status OK - 200
+        // Failure Status: BAD_REQUEST - 400 (in case user does not exist) Response: "User does not exit."
+
+
+
+
+        // Follow User
+        // POST - openchat/follow { followerId: Alice ID, followeeId: Bob ID }
+        // Success Status OK - 201
+        // Failure Status: BAD_REQUEST - 400 (in case one of the users doesn't exist) Response: "At least one of the users does not exit."
+
+
+
+
+        // Retrieve Wall
+        // GET - openchat/user//wall [{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "BOB_IDxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Planning to eat something with Charlie. Wanna join us?", "date" : "10/01/2018", "time" : "13:25:00" },{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "ALICE_ID-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Anything interesting happening tonight?", "date" : "10/01/2018", "time" : "11:30:00" },{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "BOB_IDxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "What's up everyone?", "date" : "10/01/2018", "time" : "11:20:50" },{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "CHARLIE_IDxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Hi all. Charlie here.", "date" : "10/01/2018", "time" : "09:15:34" },{ "postId" : "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "userId" : "ALICE_ID-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "text" : "Anything interesting happening tonight?", "date" : "10/01/2018", "time" : "09:00:00" }]
+        // Success Status OK - 200
+        // Failure Status: BAD_REQUEST - 400 (in case user does not exist) Response: "User does not exist."
+
+
+
+
+        // Retrieve All Users
+        // GET - openchat/users [{ "userId" : "123e4567-e89b-12d3-a456-426655440000", "username" : "Alice", "about" : "I love playing the pianno and travel.", },{ "userId" : "093f2342-e89b-12d3-a456-426655440000", "username" : "Bob", "about" : "Writer and photographer. Passionate about food and languages." },{ "userId" : "316h3543-e89b-12d3-a456-426655440000", "username" : "Charlie", "about" : "I'm a basketball player, love cycling and meeting new people. " }]
+        // Success Status OK - 200
+
+
+
+
+        // Retrieve all users followed by another user (followees)
+        // GET - openchat/user/:userId/followees [{ "userId" : "123e4567-e89b-12d3-a456-426655440000", "username" : "Alice", "about" : "I love playing the pianno and travel.", },{ "userId" : "093f2342-e89b-12d3-a456-426655440000", "username" : "Bob", "about" : "Writer and photographer. Passionate about food and languages." },{ "userId" : "316h3543-e89b-12d3-a456-426655440000", "username" : "Charlie", "about" : "I'm a basketball player, love cycling and meeting new people. " }]
+        // Success Status OK - 200
     }
 }
