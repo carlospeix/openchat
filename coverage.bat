e1cb11c7088da41c62080284bdc9e0435219bba1
Carlos Peix <carlos.peix@gmail.com>
2020-11-03 23:06:21 -0300

    Nada

--- coverage.bat
+++ coverage.bat
@@ -3,4 +3,4 @@ rem https://github.com/coverlet-coverage/coverlet/blob/master/Documentation/VSTe
 dotnet test --collect:"XPlat Code Coverage" --results-directory:Coverage rem --settings coverlet.runsettings
 
 rem https://github.com/danielpalme/ReportGenerator
-reportgenerator "-reports:Coverage\coverage.cobertura.xml" "-targetdir:Coverage\html" -reporttypes:HTML;
+reportgenerator -reports:Coverage\coverage.cobertura.*.xml -targetdir:Coverage\html -reporttypes:HTML;
